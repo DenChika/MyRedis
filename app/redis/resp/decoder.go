dd86ec4995642cef715560f7573210515f1f79ab
Денис Соколов <sokolov.d@notissimus.com>
2024-08-22 01:12:08 +0300

    debugging

--- app/redis/resp/decoder.go
+++ app/redis/resp/decoder.go
@@ -1,6 +1,7 @@
 package resp
 
 import (
+	"fmt"
 	"strings"
 )
 
@@ -10,6 +11,7 @@ func clrfSplit(str string) []string {
 
 func Decode(str string) []string {
 	split := clrfSplit(str)
+	fmt.Println(split)
 
 	result := make([]string, 0, len(split))
 	for i := 0; i < len(split); i++ {
